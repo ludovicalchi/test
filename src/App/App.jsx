f263af2f4f8e2bcb86caae529c34ced46e0b3d33
ludovic.borgnat <ludovic.borgnat@sciencespo.fr>
2023-06-12 12:17:45 +0100

    restructuration et création du Button

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Button from './components/ui/Button/Button';
 
 function App() {
   return (
     <div className="App">
       Ludo Bourgogne
+      <Button></Button>
     </div>
   );
 }
