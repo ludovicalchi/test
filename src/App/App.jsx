06c1a84369604e77539a6b9809ebb07e6e62a108
ludovic.borgnat <ludovic.borgnat@sciencespo.fr>
2023-06-12 14:23:14 +0100

    style du Button

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       Ludo Bourgogne
+      <br/>
       <Button></Button>
     </div>
   );
--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import style from './Button.module.css'
 
 const Button=(props)=>{
-    return <button>Benjamin</button>;
+    return <button className={style.Button}>Benjamin</button>;
 }
 
 export default Button
