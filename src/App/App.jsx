04ad6c7645c5c34de945301661d8a72ed6281914
ludovic.borgnat <ludovic.borgnat@sciencespo.fr>
2023-06-12 16:39:28 +0100

    css global

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       Vive la Bourgogne 
       <br/>
-      <Button bgColor="red"><img src='/img/etre-en-desaccord.png' />cancel</Button>
-      <Button bgColor="green" onClick={(unParamDeLEnfant)=>{
+      <Button className="error"><img src='/img/etre-en-desaccord.png' />cancel</Button>
+      <Button className="primary" onClick={(unParamDeLEnfant)=>{
         console.log('fonction envoyée par le parent', unParamDeLEnfant);
       }}><img src='/img/se-mettre-daccord.png' />ok</Button>
     </div>
--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -5,9 +5,12 @@ import PropTypes from 'prop-types'
 const Button=(props)=>{
     console.log(props);
     return (
-        <button style={{background: props.bgColor}} className={style.Button} onClick={(evt)=>{
-            props.onClick('click click');
-        }}>
+        <button 
+            type={props.type}
+            className={`${style.Button} btn${undefined!==props.className?' '+props.className:''}`}
+            onClick={(evt)=>{
+                props.onClick('click click');
+            }}>
             {props.children}
         </button>
     );
@@ -16,7 +19,8 @@ const Button=(props)=>{
 Button.propTypes={
     children: PropTypes.any.isRequired,
     type: PropTypes.oneOf(['button', 'reset', 'submit']).isRequired,
-    onClick: PropTypes.func.isRequired
+    onClick: PropTypes.func.isRequired,
+    className:PropTypes.oneOf(['primary', 'error'])
 }
 
 Button.defaultProps={
