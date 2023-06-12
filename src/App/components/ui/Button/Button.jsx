bb2eda43fe57d72a44a82659e1559ead81b8d24a
ludovic.borgnat <ludovic.borgnat@sciencespo.fr>
2023-06-12 15:21:38 +0100

    Ajout des propTypes et defaultProps et onclick

--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React from "react"
 import style from './Button.module.css'
+import PropTypes from 'prop-types'
 
 const Button=(props)=>{
     console.log(props);
-    return <button className={style.Button}>
-        {undefined !== props.children ?
-        props.children : props.label}</button>;
+    return (
+        <button className={style.Button} onClick={(evt)=>{
+            console.log(evt);
+            props.onClick();
+        }}>
+            {props.children}
+        </button>
+    );
+}
+
+Button.propTypes={
+    children: PropTypes.any.isRequired,
+    type: PropTypes.oneOf(['button', 'reset', 'submit']).isRequired,
+    onClick: PropTypes.func.isRequired
+}
+
+Button.defaultProps={
+    type:'button',
+    onClick: ()=>{}
 }
 
 export default Button
